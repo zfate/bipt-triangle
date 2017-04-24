4dbea16b5dab60ae031c0b558dd48753d3e4cb80
zfate <wanghaomengwhm@qq.com>
2017-04-24 12:06:52 +0800

    bipt-triangle

--- code/code/triangle/triangle.js
+++ code/code/triangle/triangle.js
@@ -9,25 +9,23 @@ var triangle = {
     		return true ;
     },
     isIsosceles : function(a,b,c) 
-	{       
-            if(a!=b) if(a!=c) if(b!=c) return false;
-	        if(a<=0) return false ;
+	{
+    	    if(a!=b) if(a!=c) if(b!=c) return false;
+			if(a<=0) return false ;
 			if(b<=0) return false ;
 			if(c<=0) return false ;
 			if(a+b<=c) return false ;
 			if(a-b>=c) return false ;
-    		return true ;
-	  
-     },
-	isScalene  : function(a,b,c)
+			return true ;
+    },
+	isScalene : function(a,b,c)
 	{
 	        if(a<=0) return false ;
 			if(b<=0) return false ;
 			if(c<=0) return false ;
-			if(a+b<=c) return false ;
+	        if(a+b<=c) return false ;
 			if(a-b>=c) return false ;
-    	    return true ;
+			return true ;
 	}
-	
 };
 
--- code/code/triangle/triangleTest.js
+++ code/code/triangle/triangleTest.js
@@ -8,14 +8,14 @@ QUnit.test("isosceles triangle has two equal sides", function( assert ) {
 	assert.equal(triangle.isIsosceles(5,5,2),true,"5,5,2 is isosceles") ;
 	assert.equal(triangle.isIsosceles(2,5,5),true,"2,5,5 is isosceles") ;
 	assert.equal(triangle.isIsosceles(5,2,5),true,"5,2,5 is isosceles") ;
-	assert.equal(triangle.isIsosceles(1,1,7),false,"1,1,7 is not isosceles") ;
-	assert.equal(triangle.isIsosceles(3,2,5),false,"3,2,5 is not isosceles") ;
-	assert.equal(triangle.isIsosceles(0,0,1),false,"0,0,1 is not isosceles") ;
-	assert.equal(triangle.isIsosceles(-1,-1,5),false,"-1,-1,5 is not isosceles") ;
+	assert.equal(triangle.isIsosceles(1,1,7),false,"1,1,7 is isosceles") ;
+	assert.equal(triangle.isIsosceles(3,2,5),false,"3,2,5 is isosceles") ;
+	assert.equal(triangle.isIsosceles(0,0,1),false,"0,0,1 is isosceles") ;
+	assert.equal(triangle.isIsosceles(-1,-1,5),false,"-1,-1,5 is isosceles") ;
 });
 QUnit.test("scalene triangle has three different sides", function( assert ) {
 	assert.equal(triangle.isScalene(3,2,4),true,"3,2,4 is scalene") ;
-	assert.equal(triangle.isScalene(1,2,6),false,"1,2,6 is not scalene") ;
+	assert.equal(triangle.isScalene(1,2,6),false,"1,2,6 is scalene") ;
 });
 
 
